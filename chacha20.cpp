70b882b487bb62636d2d61c2f8d283b3dfdcf723
acetone <acetone@mail.i2p>
2022-06-18 04:49:11 -0400

    chacha20 added

---
+++ chacha20.cpp
@@ -0,0 +1,50 @@
+/*
+ * Based on
+ * 1. OpenSSL lib
+ * 2. PurpleI2P source code
+ * 3. cppcodec lib
+ *
+ * PUBLIC DOMAIN C++ WRAPPER
+ * acetone, 2022
+ */
+
+#include "chacha20.h"
+
+#include <memory>
+#include <openssl/evp.h>
+#include <inttypes.h>
+#include <string.h>
+
+namespace FriendlyCrypto {
+
+std::vector<uint8_t> chaCha20(const std::vector<uint8_t> &msg, const std::array<uint8_t, 32> &key, const uint8_t *nonce)
+{
+    uint8_t fakenonce[24] {0};
+    if (!nonce)
+    {
+        nonce = fakenonce;
+    }
+
+    EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new ();
+    uint32_t iv[4];
+    iv[0] = htole32 (1); memcpy (iv + 1, nonce, 12); // counter | nonce
+    EVP_EncryptInit_ex(ctx, EVP_chacha20(), NULL, key.data(), reinterpret_cast<const uint8_t*>(iv));
+    std::vector<uint8_t> out(msg.size());
+    int outlen = msg.size();
+    EVP_EncryptUpdate(ctx, out.data(), &outlen, msg.data(), msg.size());
+    EVP_EncryptFinal_ex(ctx, NULL, &outlen);
+    EVP_CIPHER_CTX_free (ctx);
+    return out;
+}
+
+std::vector<uint8_t> chaCha20(const uint8_t *msg, size_t msgSize, const std::array<uint8_t, 32> &key, const uint8_t *nonce)
+{
+    std::vector<uint8_t> vector;
+    for (size_t i = 0; i < msgSize; ++i)
+    {
+        vector.push_back(msg[i]);
+    }
+    return chaCha20(vector, key, nonce);
+}
+
+} // namespace
---
+++ chacha20.h
@@ -0,0 +1,38 @@
+/*
+ * Based on
+ * 1. OpenSSL lib
+ * 2. PurpleI2P source code
+ * 3. cppcodec lib
+ *
+ * PUBLIC DOMAIN C++ WRAPPER
+ * acetone, 2022
+ */
+
+#ifndef CHACHA20_H
+#define CHACHA20_H
+
+#include <vector>
+#include <array>
+
+using uint8_t = unsigned char;
+
+namespace FriendlyCrypto {
+#if defined(__FreeBSD__) || defined(__NetBSD__)
+#include <sys/endian.h>
+
+#elif defined(__linux__) || defined(__FreeBSD_kernel__) || defined(__OpenBSD__) || defined(__GLIBC__)
+#include <endian.h>
+
+#elif defined(__APPLE__) && defined(__MACH__)
+#include <libkern/OSByteOrder.h>
+#define htole32(x) OSSwapHostToLittleInt32(x)
+
+#else
+#define htole32
+#endif
+
+std::vector<uint8_t> chaCha20(const std::vector<uint8_t>& msg, const std::array<uint8_t, 32>& key, const uint8_t * nonce = nullptr);
+std::vector<uint8_t> chaCha20(const uint8_t* msg, size_t msgSize, const std::array<uint8_t, 32>& key, const uint8_t * nonce = nullptr);
+}
+
+#endif // CHACHA20_H
